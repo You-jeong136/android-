7c658d9f2cb74f34859835d83d2a8134549f036a
You-jeong136 <smj06168@gmail.com>
2022-02-25 15:43:53 +0900

    [FEAT] firebase google auth 로그인 연동

--- firebase/app/src/main/java/com/study/aos/ui/auth/AuthGoogleFragment.kt
+++ firebase/app/src/main/java/com/study/aos/ui/auth/AuthGoogleFragment.kt
@@ -1,20 +1,118 @@
 package com.study.aos.ui.auth
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.study.aos.R
+import com.study.aos.databinding.FragmentAuthGoogleBinding
+import com.study.aos.databinding.FragmentRemoteConfigBinding
 
 class AuthGoogleFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private var _binding: FragmentAuthGoogleBinding? = null
+    private val binding get() = _binding ?: error("View를 참조하기 위해 binding이 초기화되지 않았습니다.")
+
+    private val getGoogleSignInResult : ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){ result ->
+        if(result.resultCode == RESULT_OK){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "******firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.w(TAG, "******Google sign in failed", e)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auth_google, container, false)
+        _binding = FragmentAuthGoogleBinding.inflate(inflater, container, false)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        auth = Firebase.auth
+
+        initOnClickListener()
+
+        return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "*******signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "********signInWithCredential:failure", task.exception)
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        getGoogleSignInResult.launch(signInIntent)
+    }
+
+    private fun signOut() {
+        Firebase.auth.signOut()
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        binding.tvUserEmail.text = user?.email
+        binding.tvUserName.text = user?.displayName
+    }
+
+    private fun initOnClickListener(){
+        binding.apply {
+            btnSigninGoogle.setOnClickListener {
+                signIn()
+            }
+
+            btnLogoutGoogle.setOnClickListener {
+                signOut()
+            }
+        }
+    }
+    companion object {
+        private const val TAG = "GoogleActivity"
+    }
 }
